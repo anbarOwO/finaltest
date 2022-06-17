7cfcc4d308044c9223364999d3eb67871c239f1b
Daniel <a0976008109@gmail.com>
2022-06-17 17:38:05 +0800

    test

---
+++ assets/js/register-box.js
@@ -0,0 +1,7 @@
+function copybox(){
+    for(i=1;i<=n;i++){
+        document.write('<div class="box">')
+        document.write('<img src="../assets/photo/login register/register-book.jpeg">')
+        document.write("</div>")
+        }
+}
